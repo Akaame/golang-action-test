2be1cc135fc2a4b59a0e1aefb3b3f5b458495751
siddik.acil <siddik.acil@egym.com>
2021-05-25 16:42:48 +0200

    Add Zap Logging

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Akaame/golang-action-test
 
 go 1.16
+
+require go.uber.org/zap v1.16.0
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"go.uber.org/zap"
+)
+
+func main() {
+	config := zap.NewProductionConfig()
+	logger, err := config.Build()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	logger.Info("Logger setup successfull")
 }
